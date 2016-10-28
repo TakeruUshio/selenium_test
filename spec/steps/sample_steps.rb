18b5dccc438656dbb20d57406be115bd363d5837
Takeru GH Ushio <ta-ushio@groovenauts.jp>
2016-10-28 09:32:15 +0900

    サンプルのシナリオを変更しました

--- spec/steps/sample_steps.rb
+++ spec/steps/sample_steps.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+require 'pry'
 step "Seleniumでブラウザを使用する" do
   @driver = Selenium::WebDriver.for :firefox
   @driver.manage.timeouts.implicit_wait = 30
@@ -9,19 +9,12 @@ step "Seleniumのブラウザを閉じる" do
   @driver.quit()
 end
 
-step ":url にアクセスし :text を検索する" do |url, text|
+step ":url にアクセスする" do |url|
   @driver.navigate.to url
-  sleep 5
-  i = @driver.find_element(:id, "sb_ifc0").find_element(:tag_name, "input")
-  i.clear
-  i.send_keys(text)
-
-  @driver.find_element(:class, "jhp").click
-  @driver.find_element(:class, "jsb").find_element(:tag_name, "input").click
-  sleep 10
 end
 
-step ":text と表示されること" do |text|
-  expect(@driver.find_element(:class, "med").text).to eq (text)
+step ":button ボタンが表示されていること" do |button|
+	button_list = @driver.all(:tag_name, "button")
+	expect(button_list.any?{|b| b.text == button}).to be_truthy
 end
 
