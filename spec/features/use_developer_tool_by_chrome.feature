375b7d5f3c17ba61ac2756cd6bf6988a8edcd671
TakeruUshio <ta-ushio@groovenauts.jp>
2018-10-18 18:29:51 +0900

    Make a failed test

---
+++ spec/features/use_developer_tool_by_chrome.feature
@@ -0,0 +1,9 @@
+# encoding: utf-8
+# language: ja
+
+ 機能: sample
+ シナリオ: Get chrome console log
+   もし  'chrome' でブラウザを起動する
+   かつ   'https://www.yahoo.co.jp/' にアクセスする
+   かつ   テストを失敗させる
+
--- spec/steps/sample_steps.rb
+++ spec/steps/sample_steps.rb
@@ -22,6 +22,11 @@ step ":url にアクセスする" do |url|
   @driver.navigate.to url
 end
 
+step "テストを失敗させる" do
+  expect(false).to be_truthy
+end
+
+# 以下expect系のstep
 step ":button ボタンが表示されていること" do |button|
   button_list = @driver.all(:tag_name, "button")
   expect(button_list.any?{|b| b.text == button}).to be_truthy
